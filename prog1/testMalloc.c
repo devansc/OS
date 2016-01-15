9f71e288a2cd3e37cb6a85d01b03de0d31ef9bdd
Devan Carlson <devansc@gmail.com>
2016-01-14 17:54:55 -0800

    nothing

--- prog1/testMalloc.c
+++ prog1/testMalloc.c
@@ -1,16 +1,15 @@
 #include "malloc.h"
 
 int main(int argc, char **argv) {
-    char *a = (char*) malloc(6), *b, *c;
-    strcpy(a, "hello");
-    printf("a at %p --> %s\n", a, a);
-    b = malloc(7);
-    strcpy(b, "whasup");
-    printf("b at %p --> %s\n", b, b);
+  unsigned char *val;
 
-    a = realloc(a, 200);
-    strcpy(a, "whats up now im 200");
-    printf("a at %p --> %s\n", a, a);
+  val = malloc (1024);
+  if ( val )
+    printf("Calling malloc succeeded.\n");
+  else {
+    printf("malloc() returned NULL.\n");
+    exit(1);
+  }
 
     return 0;
 }
