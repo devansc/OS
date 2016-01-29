88408aac7940f6d376faf645351f07436ad4ef17
Devan Carlson <devansc@gmail.com>
2016-01-28 19:33:26 -0800

    Prog 2 initial commit

---
+++ prog2/lwp.c
@@ -0,0 +1,151 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lwp.h"
+
+
+#define firstContext sched_one
+#define lastContext sched_two
+#define nextThread lib_one 
+#define lastThread lib_two 
+#define WORDSIZE sizeof(unsigned long)
+
+struct scheduler rrScdlr = {NULL, NULL, rr_admit, rr_remove, rr_next};
+scheduler rrScheduler = &rrScdlr;
+scheduler currentScheduler;
+
+static context systemContext;
+thread systemThread;
+
+tid_t nextTid = 1;
+thread curThread;
+
+void printStack(thread t) {
+    printf("STACK: pos %p\n", t->stack);
+    printf("\t%lu\n", t->stack[0]);
+    printf("\t%lu\n", t->stack[-1]);
+    printf("\t%lu\n", t->stack[-2]);
+}
+
+void printThreads(thread cur, tid_t last) {
+    if (!cur || cur->tid < last) return;
+    printf("\tthread %ld\n", cur->tid);
+    printThreads(cur->nextThread, cur->tid);
+}
+
+void rr_admit(thread newThread) {
+    if (systemThread->firstContext == NULL) {
+        systemThread->firstContext = systemThread->lastContext = newThread;
+        systemThread->nextThread = newThread;
+        return;
+    }
+
+    newThread->nextThread = systemThread->firstContext;
+    newThread->lastThread = systemThread->lastContext;
+    systemThread->lastContext->nextThread = newThread;
+    systemThread->firstContext->lastThread = newThread;
+    systemThread->lastContext = newThread;
+}
+
+void rr_remove(thread victim) {
+    if (victim->lastThread != NULL) {
+        victim->lastThread->nextThread = victim->nextThread;
+    }
+    if (victim->nextThread != NULL) {
+        victim->nextThread->lastThread = victim->lastThread;
+    }
+    // free(victim);  ??
+}
+
+thread rr_next() {
+    return curThread->nextThread;
+}
+
+tid_t lwp_create(lwpfun func, void *arg, size_t stackSize) {
+    unsigned long *stack = malloc(stackSize * WORDSIZE);
+    unsigned long *sp = stack + stackSize - 1;
+    rfile state;
+    thread threadSpace = malloc(sizeof(context));
+    context newContext;
+
+    if (currentScheduler == NULL) {
+        currentScheduler = rrScheduler;
+        systemThread = &systemContext;
+        curThread = systemThread;
+    }
+
+    *sp-- = (unsigned long) lwp_exit;
+    *sp-- = (unsigned long) func;
+    state.rsp = state.rbp = (unsigned long) sp;
+    state.rdi = (unsigned long) arg;
+
+    newContext = (context) {nextTid++, stack, stackSize, state, NULL, NULL,
+            NULL, NULL};  // TODO change sp to stack so I can free later
+    memcpy(threadSpace, &newContext, sizeof(context));
+    currentScheduler->admit(threadSpace);
+    return threadSpace->tid;
+}
+
+void lwp_exit() {
+    thread next = currentScheduler->next();
+    thread cur = curThread;
+    if (curThread == next) {
+        return lwp_stop();
+    }
+    curThread->lastThread->nextThread = next;
+    next->lastThread = curThread->lastThread;
+    // need to check first/lastContext in scheduler?
+    switchContext(next);
+    free(cur->stack);
+    free(cur);
+}
+
+tid_t lwp_gettid() {
+    if (curThread == NULL) 
+        return NO_THREAD;
+    return curThread->tid;
+}
+
+void lwp_stop() {
+    load_context(&systemThread->state);
+}
+
+scheduler lwp_get_scheduler() {
+    return currentScheduler;
+}
+
+void lwp_set_scheduler(scheduler new) {
+    currentScheduler = new;
+}
+
+void switchContext(thread to) {
+    thread old = curThread;
+    curThread = to;
+    swap_rfiles(&(old->state), &(to->state));
+}
+
+void lwp_yield() {
+    thread next = currentScheduler->next();
+    switchContext(next);
+}
+
+void lwp_start() {
+    thread next;
+    if (currentScheduler)
+        next = currentScheduler->next();
+    if (next)
+        switchContext(next);
+}
+
+thread tid2thread(tid_t tid) {
+    return tid2threadHelper(systemThread->firstContext, tid);
+}
+
+thread tid2threadHelper(thread cur, tid_t tid) {
+    if (cur == NULL)
+        return NULL;
+    else if (cur->tid == tid)
+        return cur;
+    else
+        return tid2threadHelper(cur->nextThread, tid);
+}
