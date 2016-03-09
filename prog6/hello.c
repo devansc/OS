3fb94ca3ad23aa99fccf02c72075d41136891042
Devan Carlson <devansc@gmail.com>
2016-03-09 13:25:06 -0800

    Added hello world

---
+++ prog6/hello.c
@@ -0,0 +1,12 @@
+/* 
+ * Author: Devan Carlson (decarlso)
+ */
+#include <stdio.h>
+
+/*
+ * Prints Hello, world! to stdout.
+ */
+int main(int argc, char **argv) {
+    printf("Hello, world!\n");
+    return 0;
+}
